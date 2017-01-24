530b390c5f6948fa39957d479401909ecdacf3b9
bensezio <benedictsefaziorklui@Benedicts-MBP.home>
2017-01-24 23:20:40 +0000

    git commit -m "Refactoring"

--- src/components/weather/WeatherPage.js
+++ src/components/weather/WeatherPage.js
@@ -11,15 +11,18 @@ class WeatherPage extends React.Component {
     this.state = {
       city: '',
       "country":"GB",
-      cnt: 0,
+      cnt: 5,
       lists: [],
-      time: ''
+      time: '',
+      day: ''
     };
   }
 
   componentWillMount() {
     // Called the first time the component is loaded right before the component is added to the Page
-   const url = "http://api.openweathermap.org/data/2.5/forecast/city?id=2648110daily?q=London,GB&cnt=5&APPID=0347c864d2bf42c0018f6c88f5869e61";
+   const url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=London,GB&cnt=5&APPID=0347c864d2bf42c0018f6c88f5869e61";
+
+ 
     $.get(url, (data) => {
       //console.warn("WEATHER API CALL DELAYS... " + data);
       this.setState({
@@ -27,35 +30,55 @@ class WeatherPage extends React.Component {
         count: data.cnt,
         country: data.city.country,
         lists: data.list,
-        time: data.list.dt
+        time: data.list.dt,
+        day: data.list.dt
       });
     });
   }
 
   render() {
     // Converting Datetime 
-    let date = this.state.time;
-    let result = moment(date).add(1, 'days').format("dddd, MMMM Do YYYY");
-
+    
+    // let start = moment([2017, 01, 24;]);
+    // let end = moment([2017, 01, 29]);
+    let date = this.state.day;
+    let result = moment(date).format("llll");
+      
     // Iterating through list data for weather results
     const lists = this.state.lists.map((list, index) => {
         return (
             <div key={index}>
-              <strong>Day: {result}</strong>
-              <h5>Main Temperature: {list['main']['temp']}</h5>
+              
+              <h4>Day: {result}</h4>
+              
+              <h5 class="label label-primary">Day Temperature: {list['temp']['day']}</h5>
+
+              <h5 class="label label-primary">Pressure: {list['pressure']}</h5>
+
+              <h5 class="label label-primary">Humidity: {list['humidity']}</h5>
+
+              <h5 class="label label-primary">Speed: {list['speed']}</h5>
+
+              <h5 class="label label-primary">Degree: {list['deg']}</h5>
+
+              <h5 class="label label-primary">Clouds: {list['clouds']}</h5>
+
               <strong>Description:</strong> <i>{list['weather'][0]['description']}</i>
-              <hr />
+            
+              <div className="border-separator">&nbsp;</div>
+
             </div>
           );
       });
 
 
     return (
-      <div className="jumbotron">
-        <h3>Please await {this.state.country} weather forecast for {this.state.city} ... </h3>        
-        <div>
-            <blockquote>Forecast for {this.state.count} days</blockquote>
-        </div>
+      <div className="jumbotron">        
+          <div className="header-text">
+              <h3>Please await {this.state.country} weather forecast for {this.state.city} ... </h3>
+          </div>
+          <blockquote>Forecast for {this.state.count} days</blockquote>
+        
         <ul>
           <li>{lists}</li>
         </ul>
